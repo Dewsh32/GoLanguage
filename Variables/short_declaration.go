5007d336dc0bce0ba34eae604c913550c6a7661c
DEVANSH GOEL <devanshg85@gmail.com>
2020-04-27 15:02:18 +0530

    short declaration

---
+++ Variables/short_declaration.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// taking a string variable
+	Language := "Go Programming"
+	//declaring and assigning
+	x := 42
+	//multiple
+	a, b, c := 11, 12, 13
+	// multiple variables of different types
+	y, z, w := "GFG", 859.24, 1234
+	fmt.Println(x)
+	fmt.Println(a, b, c)
+	fmt.Println(y, z, w)
+	fmt.Println(Language)
+}
